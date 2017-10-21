d215c92588e30044a390e27973a7c5ff0227af2e
Clay Tinnell <ctinnell@gaig.com>
2017-10-21 09:28:45 -0400

    Predictions working.

--- VideoXRay/ViewController.swift
+++ VideoXRay/ViewController.swift
@@ -162,6 +162,65 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
         if writerInput.isReadyForMoreMediaData {
             writerInput.append(sampleBuffer)
         }
+        
+        guard readyToAnalyze else { return }
+        readyToAnalyze = false
+        
+        DispatchQueue.global().async {
+            let inputSize = CGSize(width: 227.0, height: 227.0)
+            let image = CIImage(cvPixelBuffer: pixelBuffer)
+            
+            guard let resizedPixelBuffer = image.pixelBuffer(at: inputSize, context: self.context) else { return }
+            
+            let prediction = try? self.model.prediction(image: resizedPixelBuffer)
+            
+            let predictionName = prediction?.classLabel ?? "Unknown"
+            
+            print("\(self.predictions.count): \(predictionName)")
+            let timeDiff = currentTime - self.startTime
+            
+            self.predictions.append((timeDiff, predictionName))
+            
+            self.readyToAnalyze = true
+        }
     }
 }
+
+extension CIImage {
+    
+    func pixelBuffer(at size: CGSize, context: CIContext) -> CVPixelBuffer? {
+        
+        //1 - create a dictionary requesting Core Graphics compatibility
+        let attributes = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        
+        //2 - create a pixel buffer at the size our model needs
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(size.width), Int(size.height), kCVPixelFormatType_32ARGB, attributes, &pixelBuffer)
+        guard status == kCVReturnSuccess else { return nil }
+        
+        //3 - calculate how much we need to scale down our image
+        let scale = size.width / self.extent.size.width
+        
+        //4 - create a new scaled-down image using the scale we just calculated
+        let resizedImage = self.transformed(by: CGAffineTransform(scaleX: scale, y: scale))
+        
+        //5 - calculate a cropping rectangle and apply it immediately
+        let width = resizedImage.extent.width
+        let height = resizedImage.extent.height
+        let yOffset = (CGFloat(height) - size.height) / 2.0
+        let rect = CGRect(x: (CGFloat(width) - size.width) / 2.0, y: yOffset, width: size.width, height: size.height)
+        let croppedImage = resizedImage.cropped(to: rect)
+        
+        //6 - move the cropped image down so that its centered
+        let translatedImage = croppedImage.transformed(by: CGAffineTransform(translationX: 0, y: -yOffset))
+        
+        //7 - render the CIImage to our CVPixelBuffer and return it
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        context.render(translatedImage, to: pixelBuffer!)
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
+}
+
 
